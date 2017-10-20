76bdf69a4abeef24b62924ff34e4aa971a10bbf8
Lori <mclorimer@gmail.com>
2017-10-20 18:51:15 -0400

    completed the splash animations

--- project/trans_stats/urls.py
+++ project/trans_stats/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from trans_stats.views import IndexView
+from . import views
 
 urlpatterns = [
-    url(r'^trans_stats/', IndexView.as_view(), name='index')
+    url(r'^trans_stats/', IndexView.as_view(), name='index'),
+    url(r'^$', views.homeView, name="home")
 ]
--- project/trans_stats/views.py
+++ project/trans_stats/views.py
@@ -5,3 +5,6 @@ class IndexView(View):
 	
 	def get(self, request):
 		return render(request, 'trans_stats/index.html')
+
+def homeView(request):
+	return render(request, 'trans_stats/welcome.html')
